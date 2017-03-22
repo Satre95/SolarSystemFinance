84206903d7e9f233b886816daa2f62837016a125
Siddharth Atre <sidatre@gmail.com>
2017-03-22 14:29:05 -0700

    Edits preparing for billboards later on

--- bin/data/CelestialShaderVert.glsl
+++ bin/data/CelestialShaderVert.glsl
@@ -4,9 +4,15 @@ in vec4 position;
 in vec4 color;
 
 out vec4 fragColor;
+
+uniform mat4 orientationMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
 uniform mat4 modelViewProjectionMatrix;
 
 void main() {
 	gl_Position = modelViewProjectionMatrix * position;
 	fragColor = color;
+
+
 }
