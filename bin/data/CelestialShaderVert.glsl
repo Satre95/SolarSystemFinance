4bd6390c2d04d0e62f7e74d4b5a3d805469eb1a2
Siddharth Atre <sidatre@gmail.com>
2017-03-22 13:39:57 -0700

    Renamed shaders

--- bin/data/CelestialShaderVert.glsl
+++ bin/data/CelestialShaderVert.glsl
@@ -3,5 +3,8 @@
 layout (location = 0) in vec3 position;
 layout (location = 0) in vec4 color;
 
+uniform mat4 modelViewProjectionMatrix;
+
 void main() {
+	gl_Position = modelViewProjectionMatrix * vec4(position, 1.0f);
 }
