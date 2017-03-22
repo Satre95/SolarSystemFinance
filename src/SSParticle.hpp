d9616dd9cd6079c54b70bf0c6e4e65fa8b810717
Siddharth Atre <sidatre@gmail.com>
2017-03-22 14:09:33 -0700

    changed position to be vec4

--- src/SSParticle.hpp
+++ src/SSParticle.hpp
@@ -4,16 +4,17 @@
 struct SSParticle
 {
 	SSParticle() {
-		pos = vel = force = ofVec3f(0);
+		pos = ofVec4f(0, 0, 0, 1.0f);
+		vel = force = ofVec3f(0);
 		color = ofFloatColor(1.0f);
 	}
 
 	SSParticle(ofVec3f pos) {
-		this->pos = pos;
+		this->pos = ofVec4f(pos.x, pos.y, pos.z, 1.0f);
 		vel = force = ofVec3f(0);
 		color = ofFloatColor(1.0f);
 	}
-	ofVec3f pos;
+	ofVec4f pos;
 	ofVec3f vel;
 	ofVec3f force;
 	ofFloatColor color;
--- src/SSPlanet.hpp
+++ src/SSPlanet.hpp
@@ -3,17 +3,18 @@
 
 struct SSPlanet {
 	SSPlanet() {
-		pos = vel = ofVec3f(0);
-		color = ofFloatColor(1.0f, 0, 0, 1.0f);
-	}
-
-	SSPlanet(ofVec3f pos) {
-		this->pos = pos;
+		pos = ofVec4f(0, 0, 0, 1.0f);
 		vel = ofVec3f(0);
 		color = ofFloatColor(1.0f, 0, 0, 1.0f);
 	}
 
-	ofVec3f pos;
+	SSPlanet(ofVec3f pos) {
+		this->pos = ofVec4f(pos.x, pos.y, pos.z, 1.0f);
+		vel = ofVec3f(0);
+		color = ofFloatColor(1.0f, 0, 0, 1.0f);
+	}
+
+	ofVec4f pos;
 	ofVec3f vel;
 	ofFloatColor color;
 };
